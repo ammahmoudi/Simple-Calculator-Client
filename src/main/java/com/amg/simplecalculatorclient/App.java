63d3969d88da92925e80733e836fbcc77eedba33
Amirhossein Mahmoudi <95019899+ammahmoudi@users.noreply.github.com>
2022-04-12 16:42:07 +0430

    adding server handeler

--- src/main/java/com/amg/simplecalculatorclient/App.java
+++ src/main/java/com/amg/simplecalculatorclient/App.java
@@ -11,10 +11,14 @@ public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("client-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 520, 340);
+        Scene scene = new Scene(fxmlLoader.load(), 620, 440);
         stage.setTitle("Client Calculator");
         stage.setScene(scene);
         stage.show();
+    }
+    @Override
+    public void stop() {
+        System.exit(0);
     }
 
     public static void main(String[] args) {
